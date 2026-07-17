17e01b52f8144ca59c8ad77ee26a7690ef80b3e7
khobby-specs <specialaddo22@gmail.com>
2026-07-17 13:04:44 +0000

    Word reverser: Reverses letters in word but preserves order of appearance in sentence(comment update)

--- phase-1/python/personal-scripts/reverse-word.py
+++ phase-1/python/personal-scripts/reverse-word.py
@@ -4,9 +4,9 @@ words = input("Enter Sentence: ").split()
 #My approach: loops through each word in the list and prints out the reverse order of the word using the negative index method.
 #Immediately i use the end keyword for print to avoid printing on a new line for each word so the sentence is reserved
 #with this, the end always prints and extra space at the end of the sentence
-"""for word in words:
-    print(word[::-1], end=" ")
-print()"""
+#for word in words:
+    #print(word[::-1], end=" ")
+#print()
 
 for word in words:
     result = "".join(reversed(word))
