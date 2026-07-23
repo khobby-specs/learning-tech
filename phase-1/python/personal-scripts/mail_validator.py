01afc85c7cda0ec32154a810b37c41982dea3225
khobby-specs <specialaddo22@gmail.com>
2026-07-23 20:51:08 +0000

    Mail Validator upgrade: loop running, Pyfiglet added

--- phase-1/python/personal-scripts/mail_validator.py
+++ phase-1/python/personal-scripts/mail_validator.py
@@ -1,8 +1,21 @@
+#Requirements: this script comes with a 3rd party module.
+#To install it, please run 'pip install pyfiglet' in your virtual environment or terminal.
 from email_validator import validate_email, EmailNotValidError
 import csv
+import os
+import pyfiglet
+
 
 CSV_FILE = "tested_mails.csv"
-print("\n ---Welcome to email validator--- ")
+welcome = pyfiglet.figlet_format("--- Welcome to email validator --- ", font='digital')
+print(welcome)
+print("--- To exit, press ctrl+D\n")
+
+if not os.path.exists(CSV_FILE):
+    with open(CSV_FILE, mode="w", newline="") as file:
+        writer = csv.writer(file)
+        writer.writerow(["Name", "Email"])
+
 while True:
     try:
         name = input("Enter Name: ")
@@ -14,8 +27,13 @@ while True:
         with open(CSV_FILE, mode="a") as file:
             writer = csv.writer(file)
             writer.writerow([name, normalized_email])
+    except EOFError:
+        exit = pyfiglet.figlet_format("--- Program exited --- ", font='digital')
+        print(f"\n{exit}")
         break
     except EmailNotValidError:
+        print()
         print("Email not valid")
         continue
         
+
